0bd770e2a125a459af051f1b2b86d0dfed5ff829
aldoub <aldoub@iu.edu>
2024-03-26 18:17:58 -0400

    Added Authentication

---
+++ src/main/java/edu/iu/aldoub/primesservice/service/IAuthenticationService.java
@@ -0,0 +1,10 @@
+package edu.iu.aldoub.primesservice.service;
+
+import edu.iu.aldoub.primesservice.model.Customer;
+
+import java.io.IOException;
+
+public interface IAuthenticationService {
+    boolean register(Customer customer) throws IOException;
+    boolean login(String username, String password) throws IOException;
+}
